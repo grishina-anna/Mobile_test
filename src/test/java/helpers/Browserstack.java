f143911e060e923b9c5dc3567f3e5bc85e386caf
grish <grishina@factor-ts.ru>
2022-03-22 23:41:49 +0300

    allure

---
+++ src/test/java/helpers/Browserstack.java
@@ -0,0 +1,17 @@
+package helpers;
+
+import static io.restassured.RestAssured.given;
+
+public class Browserstack {
+    public static String videoUrl(String sessionId) {
+        return given()
+                .auth().basic("bsuser_lhwI3G", "zQg8pVwyA1cKCyGoKSU8")
+                .when()
+                .get("https://api-cloud.browserstack.com/app-automate/sessions/" + sessionId + ".json")
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("automation_session.video_url");
+    }
+}
+
